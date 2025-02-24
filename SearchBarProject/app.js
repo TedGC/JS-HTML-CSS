433755416d49f4dbf47116282761bbdd7451c42f
TedGC <90265172+TedGC@users.noreply.github.com>
2025-02-24 22:08:05 +0900

    Update app.js

--- SearchBarProject/app.js
+++ SearchBarProject/app.js
@@ -1,1 +1,26 @@
 
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '1dc5ecec',
+            s: searchTerm
+        }
+    })
+
+    console.log(response.data);
+}
+
+// debouncing an input 
+const input = document.querySelector('input')
+let timeoutId
+const onInput = e => {
+    if (timeoutId) {
+        clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(e.target.value)
+    }, 500)
+}
+
+
+input.addEventListener('input', onInput)
