04dc84f2b91c8969af973227428144bd1ebdf177
TedGC <90265172+TedGC@users.noreply.github.com>
2025-02-25 15:48:31 +0900

    Update app.js

--- SearchBarProject/app.js
+++ SearchBarProject/app.js
@@ -9,57 +9,17 @@ const fetchData = async (searchTerm) => {
     return repsonse.data.Search
 }
 
-const root = document.querySelector('.')
-root.innerHTML = `
-    <label><b> Serach for a moive</b></label>
-    <input class="input"/>
-    <div class="dropdown">
-        <div class="dropdwon-menu">
-            <div class="dropdwon-content reults"></div>
-        </div>
-    </div>
-`;
-// debouncing an input 
-const input = document.querySelector('input')
-const dropdown = document.querySeletor('.dropdown');
-const resultsWrapper = document.querySelector('.results')
-
-
-const onInput = aysnc e => {
-    const movies = await fetchData(e.target.value);
-
-    if(!movies.lengt) {
-        dropdonw.classList.remove('is-active');
-        return;
-    }
-    
-    resultsWrapper.innerHTML ='';
-    dropdown.classList.add('is-active')
-    for (let movie of movies) {
-        const option = document.createElement('a');
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        option.classList.add('dropdown-item')
-        option.innerHTML= `
-            <img src="${imgSrc}" />
-            ${movie.Title} ` 
-
-        option.addEventListener('click', () =>{
-            dropdown.classList.remove('is-active')
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        }
-        resultsWrapper.appendChild(option)
-    }
-}
-input.addEventListener('input', debounce(onInput, 500))
-
-document.addEventListener('click', event ()=>{
-    if(!root.contains(event.target)) {
-        dropdown.classList.remove('is-active')
-    }
+createAutoComplete({
+    root: document.querySelecor('.autocomplete')
 })
 
+createAutoComplete({
+    root: document.querySelecor('.autocomplete-two')
+})
 
+createAutoComplete({
+    root: document.querySelecor('.autocomplete-three')
+})
 const onMovieSelect = async movie=>{
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
