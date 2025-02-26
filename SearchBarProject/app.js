19ee7142516f2bb6c17d5814f598c0930ff3039d
TedGC <90265172+TedGC@users.noreply.github.com>
2025-02-26 10:18:02 +0900

    Update app.js

--- SearchBarProject/app.js
+++ SearchBarProject/app.js
@@ -1,86 +1,96 @@
-
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '1dc5ecec',
-            s: searchTerm
-        }
-    })
-    return repsonse.data.Search
-}
-
-createAutoComplete({
-    root: document.querySelector('.autocomplete', 
-    renderOption(movie){
+const autoCompleteConfig = {
+    renderOption(movie) {
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         return `
-            <img src="${imgSrc}" />
-            ${movie.Title} (${movie.year})
-            `
+        <img src="${imgSrc}" />
+                ${movie.Title} (${movie.Year}) `
     },
-    onOptionSelect(movie) {
-        onMovieSelect(movie);
+
+    inputValue(movie) {
+        return movie.Title;
+    },
+
+    async fetchData(searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: 'd9835cc5',
+                s: searchTerm
+            }
+        });
+
+        if (response.data.Error) {
+            return [];
+        }
+
+        return response.data.Search;
+        ;
     }
-                               )
-})
+}
 
 createAutoComplete({
-    root: document.querySelecor('.autocomplete-two')
-})
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#left-summary'))
+    }
+});
 
 createAutoComplete({
-    root: document.querySelecor('.autocomplete-three')
-})
-const onMovieSelect = async movie=>{
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#right-summary'))
+    }
+});
+
+const onMovieSelect = async (movie, summaryElement) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
-            apikey: '1dc5ecec',
+            apikey: 'd9835cc5',
             i: movie.imdbID
         }
-    })
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
-}
+    });
 
-const movieTemplate = (movieDetail) =>{
+    summaryElement.innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
     return `
     <article class="media">
-        <figure class="media-left">
-            <p class="image">
-                <img scr="${movieDetail.Poster}" />
-            </p>
-        </figure>
-        <div class="media-content">
-            <div class="content">
-                <h1>${movieDetail.Title}</h1>
-                <h4>${movieDetail.Genre}</h4>
-                <p>${movieDetail.Plot}</p>
-            </div>
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
         </div>
+      </div>
     </article>
     <article class="notification is-primary">
-        <p class="title">${movieDetail.Awards}</p>
-        <p class="substitle">Awards</p>
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
     </article>
-        <article class="notification is-primary">
-        <p class="title">${movieDetail.BoxOffice}</p>
-        <p class="substitle">BoxOffice</p>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
     </article>
-        <article class="notification is-primary">
-        <p class="title">${movieDetail.Metascore}</p>
-        <p class="substitle">Metascore</p>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
     </article>
-        <article class=
-            <article class="notification is-primary">
-        <p class="title">${movieDetail.imdbRating}</p>
-        <p class="substitle">IMDB Rating</p>
-    </article>"notification is-primary">
-        <p class="title">${movieDetail.imdbVotes}</p>
-        <p class="substitle">IMDB Votes </p>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
     </article>
-    
-}
-
-
-
-
-
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `;
+};
