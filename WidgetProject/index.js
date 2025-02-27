63bf3ee63391e4c2675ab5a253599f436d2672bd
TedGC <90265172+TedGC@users.noreply.github.com>
2025-02-27 21:49:52 +0900

    update

--- WidgetProject/index.js
+++ WidgetProject/index.js
@@ -1,6 +1,8 @@
-const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter; 
+const {Engine, Render, Runner, World, Bodies } = Matter; 
 
-const width = 800;
+
+const cells = 3;
+const width = 600;
 const height = 600;
 
 
@@ -19,33 +21,35 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine); 
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: MouseConstraint.create(render.canvas)
-}))
+
 // walls 
+// have to rewatch the video to understand the logic here 
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true}),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true}),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true})
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true}),
+    Bodies.rectangle(width / 2 , height, width, 40, { isStatic: true}),
+    Bodies.rectangle(0, height / 2 , 40, height, { isStatic: true}),
+    Bodies.rectangle(width, height / 2 , 40, height, { isStatic: true})
 ]
 World.add(world, walls);
 
-for (let i=0; i < 20; i++) {
-    if(Math.random() > 0.5) {
-        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50))
-    }
-    else {
-        World.add(
-            world, 
-            Bodies.circle(
-                World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 35, {
-                    redner: {
-                        fillStyle: 'green'
-                    }
-                }))
 
-            )
-        )
-    }
+//Maze generation 
+//need to understand why we use map method here to create arrays within the array
+const grid = Array(cells)
+    .fill(null)
+    .map(()=> Array(cells).fill(false))
+
+const verticals = Array(cells)
+.fill(null)
+.map(()=> Array(cells - 1).fill(false))
+
+const horizontals = Array(cells - 1)
+.fill(null)
+.map(()=> Array(cells).fill(false))
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+    
 }
