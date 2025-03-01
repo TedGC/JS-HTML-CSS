35a6a9dbfd7f10b574b336d586f344597165e19b
GC Ted <rhkdtn2056@gmail.com>
2025-03-01 20:57:10 +0900

    update sir

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,6 +1,96 @@
+console.log(err);
+
 const fs = require('fs')
+const util = require('util')
+const chalk = require('chalk')
 
-fs.readdir('.', (err) => {
+//Method #3
+// const {lstat} = fs.promises;
 
-});
 
+// Method #2
+const lstat = util.promisify(fs.lstat);
+
+// fs.readdir(process.cwd(), async (err, filenames) => {
+//     if (err) {
+//         console.log(err);
+
+//         for (let filename of filenames) {
+
+//             try {
+//                 const stats = await lstat(filename);
+
+//                 console.log(filename, stats.isFile());
+//             } catch (err) {
+//                 console.log(err);
+//             }
+
+
+//         }
+//     }
+// })
+
+const statPromises = filenames.map(filename => {
+    return lstat(filename);
+})
+
+const allStats = await Promise.all(statPromises);
+
+for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    console.log(filenames[index], stats.isFile());
+}
+
+
+
+
+
+
+//Method #1
+
+// const lstat = (filename) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if (err){
+//                 reject(err);
+//             }
+//             resolve(stats);
+//         })
+//     })
+// }
+
+
+
+
+
+
+
+
+//     const allStats = Array[filenames.length].fill(null);
+
+//     for (let filename of filenames) {
+
+//         const index = filenames.indexOf(filename);
+
+//         fs.lstat(filename, (err, stats) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             allStats[index] = stats;
+
+//             const ready = allStats.every(stats => {
+//                 return stats;
+//             })
+
+//             if (ready) {
+//                 allStats.forEach((stats, index) => {
+//                     console.log(filenames[index], stats.isFile())
+//                 })
+//             }
+
+
+
+//         })
+//     }
+// })
