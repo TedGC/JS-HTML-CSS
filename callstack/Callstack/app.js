878ea2bc14a6c1ad91a3e50082c1544a52c597ed
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-26 23:18:25 +0900

    update

---
+++ callstack/Callstack/app.js
@@ -0,0 +1,26 @@
+const multiply = (x, y) => x * y;
+
+const square = x => multiply(x, x);
+
+const isRightTriangle = (a, b, c) => (
+    square(a) + square(b) === square(c)
+)
+console.log("BEFORE")
+isRightTriangle(3, 4, 5)
+
+console.log("DONEEEE!")
+
+
+/**
+ * 
+ * 1. browsers come with Web AIPIs that are able to handle certain tasks in the background
+ * (like making requests or setTimeout)
+ * 
+ * 2. the JS call call stack recognizes these Web API functions and passes them off to the 
+ * browser to take care of 
+ * 3. Once the browser finishes those tasks, they return and are pushed onto the stack
+ * as a callback 
+ * 
+ * 
+ * 
+ */
