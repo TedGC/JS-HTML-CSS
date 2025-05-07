6ddcc0afcae6be03bdf7b6126cadc6c4c5b0067c
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-07 23:32:21 +0900

    update

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -201,4 +201,20 @@ class TimeComponent extends Component {
       </>
     );
   }
+
+
+  import React, { Component } from "react";
+import logo from "./react_photo-goose.jpg";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="logo">
+          <img src={logo} width="100%" alt="Googe Pic" />
+        </div>
+      </div>
+    );
+  }
+}
   
