bc66b32d4aee36b53431233fe28ec61c521cbec6
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-07 23:34:38 +0900

    update

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -295,3 +295,29 @@ const App = () => {
     </section>
   );
 }
+
+
+class App extends React.Component {
+    state = {
+      email: ""
+    };
+  
+    handleChange = (e) => {
+      this.setState({
+        email: e.target.value
+      });
+    };
+  
+    render() {
+      return (
+        <div>
+          <input
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <button disabled={this.state.email.length < 1}>Submit</button>
+        </div>
+      );
+    }
+  }
