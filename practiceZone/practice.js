8da74c569b361431b77d14d9be9d68507b78a64d
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-08 23:10:58 +0900

    updaet

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -392,3 +392,25 @@ class App extends React.Component {
     
       return resData;
     }
+
+
+
+    import bodyParser from 'body-parser';
+import express from 'express';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/meals', async (req, res) => {
+  const meals = await fs.readFile('./data/available-meals.json', 'utf8');
+  res.json(JSON.parse(meals));
+});
