2781b45a983a7d6950ba1a3e50e06cab4180e77f
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-08 23:10:24 +0900

    update

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -376,3 +376,19 @@ class App extends React.Component {
       },
       [url, config]
     );
+
+
+
+    async function sendHttpRequest(url, config) {
+      const response = await fetch(url, config);
+    
+      const resData = await response.json();
+    
+      if (!response.ok) {
+        throw new Error(
+          resData.message || 'Something went wrong, failed to send request.'
+        );
+      }
+    
+      return resData;
+    }
