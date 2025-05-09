1ce69b1bc114e224a5ded23d0804d3467fd91116
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-09 23:11:09 +0900

    update

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -557,3 +557,33 @@ function App() {
     </>
   );
 }
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/events" />,
+  },
+  {
+    path: '/events',
+    element: <Events />,
+    children: [
+      {
+        path: '/events/new',
+        element: <NewEvent />,
+      },
+    ],
+  },
+  {
+    path: '/events/:id',
+    element: <EventDetails />,
+    children: [
+      {
+        path: '/events/:id/edit',
+        element: <EditEvent />,
+        loader: editEventLoader,
+        action: editEventAction
+      },
+    ],
+  },
+]);
