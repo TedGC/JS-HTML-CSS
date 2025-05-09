c038c90d7df9b1b16dc86a4fb89b3641e4906fe1
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-09 23:11:29 +0900

    update

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -587,3 +587,19 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+
+
+export default function Header({ children }) {
+  const fetching = useIsFetching();
+  return (
+    <>
+      <div id="main-header-loading">{fetching > 0 && <progress />}</div>
+      <header id="main-header">
+        <div id="header-title">
+          <h1>React Events</h1>
+        </div>
+        <nav>{children}</nav>
+      </header>
+    </>
+  );
+}
