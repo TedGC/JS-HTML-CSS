f9bfb6600a9f3875ba9b45402b49e7307c5ab331
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-12 23:27:28 +0900

    update

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -948,4 +948,27 @@ function EventsList({events}) {
   );
 }
 
+function MainNavigation() {
+  const token = useRouteLoaderData('root');
 
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/events"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
