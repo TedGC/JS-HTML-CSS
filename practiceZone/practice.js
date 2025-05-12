b5b86670bc81236275b3cfd183759a445039d8de
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-12 23:26:04 +0900

    update

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -906,3 +906,22 @@ function EventItem({ event }) {
     }
   }
 
+
+  return (
+    <article className={classes.event}>
+      <img src={event.image} alt={event.title} />
+      <h1>{event.title}</h1>
+      <time>{event.date}</time>
+      <p>{event.description}</p>
+      {token && (
+        <menu className={classes.actions}>
+          <Link to="edit">Edit</Link>
+          <button onClick={startDeleteHandler}>Delete</button>
+        </menu>
+      )}
+    </article>
+  );
+}
+
+export default EventItem;
+
