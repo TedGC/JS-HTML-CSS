2991910885167fbea20467e6aed08bbd838cd6b3
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-12 23:26:20 +0900

    update

--- practiceZone/practice.js
+++ practiceZone/practice.js
@@ -925,3 +925,27 @@ function EventItem({ event }) {
 
 export default EventItem;
 
+function EventsList({events}) {
+  // const events = useLoaderData();
+
+  return (
+    <div className={classes.events}>
+      <h1>All Events</h1>
+      <ul className={classes.list}>
+        {events.map((event) => (
+          <li key={event.id} className={classes.item}>
+            <Link to={`/events/${event.id}`}>
+              <img src={event.image} alt={event.title} />
+              <div className={classes.content}>
+                <h2>{event.title}</h2>
+                <time>{event.date}</time>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
