d8fa17d74d1decc85a1aac6ddcab7ca8704349ee
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-13 22:31:16 +0900

    update

--- practiceZone/secondPractice.js
+++ practiceZone/secondPractice.js
@@ -54,3 +54,25 @@ function DynamicForm() {
 }
 
 }
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment': return { count: state.count + 1 };
+    case 'decrement': return { count: state.count - 1 };
+    default: return state;
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <span>{state.count}</span>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+    </div>
+  );
+}
