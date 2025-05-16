7f480bf3fb88113de47635ae5c8e08026c138ec2
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-16 23:08:12 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -125,3 +125,22 @@ function useWindowSize() {
 
   return size;
 }
+
+import { useEffect, useRef, useState } from 'react';
+
+function useOnScreen(options) {
+  const ref = useRef();
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisible(entry.isIntersecting);
+    }, options);
+    if (ref.current) observer.observe(ref.current);
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, [ref, options]);
+
+  return [ref, visible];
+}
