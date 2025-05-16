507e2092fe172982b02e973f35f2b3eb14432c4d
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-16 23:08:02 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -111,3 +111,17 @@ function useScrollPosition() {
 
   return scrollY;
 }
+
+import { useEffect, useState } from 'react';
+
+function useWindowSize() {
+  const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+
+  useEffect(() => {
+    const handleResize = () => setSize([window.innerWidth, window.innerHeight]);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return size;
+}
