9d621441d8878b1416a879e75aa20870206cc7a9
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-16 23:06:30 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -7,3 +7,16 @@ function usePrevious(value) {
   }, [value]);
   return ref.current;
 }
+
+import { useState, useEffect } from 'react';
+
+function useDebounce(value, delay = 500) {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debounced;
+}
