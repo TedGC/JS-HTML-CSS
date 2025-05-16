63215d987701008d4df401da57bf5fcb762945ea
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-16 23:07:43 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -81,3 +81,19 @@ function useForm(initialValues, validate) {
     handleChange
   };
 }
+
+import { useState } from 'react';
+
+function useLocalStorage(key, defaultValue) {
+  const [value, setValue] = useState(() => {
+    const stored = localStorage.getItem(key);
+    return stored ? JSON.parse(stored) : defaultValue;
+  });
+
+  const setStoredValue = val => {
+    setValue(val);
+    localStorage.setItem(key, JSON.stringify(val));
+  };
+
+  return [value, setStoredValue];
+}
