ae1bf0018a97deac400d9148885ff75e904828b8
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-19 23:12:29 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -510,3 +510,15 @@ function MultiStepForm() {
     </div>
   );
 }
+
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
