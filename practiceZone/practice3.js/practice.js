8521e4b29ad9185b1268913af3bde844a8cc42d0
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-19 23:11:20 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -388,3 +388,23 @@ function TodoList() {
     </div>
   );
 }
+
+import { useEffect, useState } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <ul>
+      {users.map((u) => (
+        <li key={u.id}>{u.name}</li>
+      ))}
+    </ul>
+  );
+}
