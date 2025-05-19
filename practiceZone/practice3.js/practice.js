e0b3e996e6ec7aa99e496076b3884c0ea5a0a5b3
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-19 23:12:07 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -466,3 +466,17 @@ function ModalExample() {
     </div>
   );
 }
+
+import { useState, useEffect } from "react";
+
+function MouseTracker() {
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handler = (e) => setPos({ x: e.clientX, y: e.clientY });
+    window.addEventListener("mousemove", handler);
+    return () => window.removeEventListener("mousemove", handler);
+  }, []);
+
+  return <p>Mouse: {pos.x}, {pos.y}</p>;
+}
