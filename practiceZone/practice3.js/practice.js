814319821ceba2ed953e1643222cf077012f39ca
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-20 22:57:17 +0900

    updte

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -739,4 +739,20 @@ class LRUCache {
     if (!this.cache.has(key)) return -1;
     const value = this.cache.get(key);
     this.cache.delete(key);
-    this.cache.set(ke
+    this.cache.set(ke    return value;
+  }
+
+  put(key, value) {
+    if (this.cache.has(key)) this.cache.delete(key);
+    else if (this.cache.size >= this.limit) this.cache.delete(this.cache.keys().next().value);
+    this.cache.set(key, value);
+  }
+}
+
+// Usage
+const cache = new LRUCache(2);
+cache.put('a', 1);
+cache.put('b', 2);
+console.log(cache.get('a')); // 1
+cache.put('c', 3);
+console.log(cache.get('b')); // -1 (evicted)
