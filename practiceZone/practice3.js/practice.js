ea719122b56107d3f45fa7d10d9ea4f1857bf782
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-24 23:10:04 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -860,3 +860,22 @@ export default function App() {
     </>
   );
 }
+
+
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error, info);
+  }
+
+  render() {
+    return this.state.hasError ? <h1>Something went wrong.</h1> : this.props.children;
+  }
+}
