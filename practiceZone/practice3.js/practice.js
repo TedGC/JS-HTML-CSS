0714b951f6b186ce1949303f3b624fe7b1b6d65f
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-25 23:07:41 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -1003,3 +1003,17 @@ const useFetch = (url) => {
 };
 
 {loading ? <Spinner /> : posts.map(p => <BlogCard key={p.id} post={p} />)}
+
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
