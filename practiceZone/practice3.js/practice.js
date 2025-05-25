e6a52cfe77148bde6533b39fdaa49b8fd4e2f5a9
TedGC <90265172+TedGC@users.noreply.github.com>
2025-05-25 23:05:48 +0900

    update

--- practiceZone/practice3.js/practice.js
+++ practiceZone/practice3.js/practice.js
@@ -982,3 +982,13 @@ export default function ScrollToTop() {
   useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
   return null;
 }
+
+
+const Button = ({ children, onClick }) => (
+  <button
+    onClick={onClick}
+    className="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700"
+  >
+    {children}
+  </button>
+);
